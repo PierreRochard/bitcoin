1a19443f582c8df0369bf5d79372591fc25b1471
Pierre Rochard <pierre@rochard.org>
2018-09-10 14:10:11 -0400

    Add walletdir unit tests for wallet init

--- src/wallet/test/init_tests.cpp
+++ src/wallet/test/init_tests.cpp
@@ -5,16 +5,18 @@
 #include <boost/test/unit_test.hpp>
 
 #include <test/test_bitcoin.h>
+#include <wallet/test/init_test_fixture.h>
+
 #include <wallet/wallet.h>
 #include <wallet/init.cpp>
 
 
-BOOST_FIXTURE_TEST_SUITE(init_tests, TestingSetup)
+BOOST_FIXTURE_TEST_SUITE(init_tests, InitTestingSetup)
 
 BOOST_AUTO_TEST_CASE(walletinit_verify_disablewallet_true)
 {
     gArgs.ForceSetArg("-disablewallet", "1");
-    gArgs.ForceSetArg("-walletdir", "pathdoesnotexist");
+    SetWalletDir(m_walletdir_path_cases["nonexistent"]);
     bool result = g_wallet_init_interface.Verify();
     BOOST_CHECK(result == true);
 }
@@ -22,59 +24,37 @@ BOOST_AUTO_TEST_CASE(walletinit_verify_disablewallet_true)
 BOOST_AUTO_TEST_CASE(walletinit_verify_disablewallet_false)
 {
     gArgs.ForceSetArg("-disablewallet", "0");
-    gArgs.ForceSetArg("-walletdir", "pathdoesnotexist");
+    SetWalletDir(m_walletdir_path_cases["nonexistent"]);
     bool result = g_wallet_init_interface.Verify();
     BOOST_CHECK(result == false);
 }
 
 BOOST_AUTO_TEST_CASE(walletinit_verify_disablewallet_default)
 {
-    gArgs.ForceSetArg("-walletdir", "pathdoesnotexist");
+    SetWalletDir(m_walletdir_path_cases["nonexistent"]);
     bool result = g_wallet_init_interface.Verify();
     BOOST_CHECK(result == DEFAULT_DISABLE_WALLET);
 }
 
 BOOST_AUTO_TEST_CASE(walletinit_verify_walletdir_does_not_exist)
 {
-    gArgs.ForceSetArg("-walletdir", "pathdoesnotexist");
+    SetWalletDir(m_walletdir_path_cases["nonexistent"]);
     bool result = g_wallet_init_interface.Verify();
     BOOST_CHECK(result == false);
 }
 
 BOOST_AUTO_TEST_CASE(walletinit_verify_walletdir_is_not_directory)
 {
-    fs::path data_dir = SetDataDir("verify_walletdir");
-    gArgs.ForceSetArg("-walletdir", "pathdoesnotexist");
+    SetWalletDir(m_walletdir_path_cases["file"]);
     bool result = g_wallet_init_interface.Verify();
     BOOST_CHECK(result == false);
 }
 
-/** If a user passes in a path with a trailing separator as the walletdir, multiple BerkeleyEnvironments
- * may be created in the same directory which can lead to data corruption.
- */
-//BOOST_AUTO_TEST_CASE(walletdir_trailing_separator)
-//{
-//    fs::path expected_wallet_dir = SetDataDir("get_wallet_dir");
-//    std::string trailing_separators;
-//    for (int i = 0; i < 4; ++i) {
-//        trailing_separators += fs::path::preferred_separator;
-//        gArgs.ForceSetArg("-walletdir", (expected_wallet_dir / fs::path::preferred_separator).string());
-//        fs::path wallet_dir = GetWalletDir();
-//        BOOST_CHECK(wallet_dir == expected_wallet_dir);
-//    }
-//}
-//
-//BOOST_AUTO_TEST_CASE(walletdir_multiple_trailing_separators)
-//{
-//    fs::path expected_wallet_dir = SetDataDir("get_wallet_dir");
-//    std::string trailing_separators;
-//    for (int i = 0; i < 4; ++i) {
-//        trailing_separators += fs::path::preferred_separator;
-//        gArgs.ForceSetArg("-walletdir", (expected_wallet_dir / trailing_separators).string());
-//        fs::path wallet_dir = GetWalletDir();
-//        BOOST_CHECK(wallet_dir == expected_wallet_dir);
-//    }
-//}
-
+BOOST_AUTO_TEST_CASE(walletinit_verify_walletdir_is_not_relative)
+{
+    SetWalletDir(m_walletdir_path_cases["relative"]);
+    bool result = g_wallet_init_interface.Verify();
+    BOOST_CHECK(result == false);
+}
 
 BOOST_AUTO_TEST_SUITE_END()
