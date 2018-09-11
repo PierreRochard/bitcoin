83182b078f264b3ea41157d34db5074a2a64e61d
Pierre Rochard <pierre@rochard.org>
2018-09-10 20:49:17 -0400

    wallet: Add walletdir arg unit tests

---
+++ src/wallet/test/init_test_fixture.cpp
@@ -0,0 +1,32 @@
+// Copyright (c) 2018 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <fs.h>
+
+#include <wallet/test/init_test_fixture.h>
+
+InitWalletDirTestingSetup::InitWalletDirTestingSetup(const std::string& chainName): BasicTestingSetup(chainName)
+{
+    std::string sep;
+    sep += fs::path::preferred_separator;
+
+    m_datadir = SetDataDir("tempdir");
+    fs::current_path(m_datadir);
+
+    m_walletdir_path_cases["default"] = m_datadir / "wallets";
+    m_walletdir_path_cases["custom"] = m_datadir / "my_wallets";
+    m_walletdir_path_cases["nonexistent"] = m_datadir / "path_does_not_exist";
+    m_walletdir_path_cases["file"] = m_datadir / "not_a_directory.dat";
+    m_walletdir_path_cases["relative"] = "wallets";
+
+    fs::create_directories(m_walletdir_path_cases["default"]);
+    fs::create_directories(m_walletdir_path_cases["custom"]);
+    fs::create_directories(m_walletdir_path_cases["relative"]);
+    std::ofstream f(m_walletdir_path_cases["file"].BOOST_FILESYSTEM_C_STR);
+}
+
+void InitWalletDirTestingSetup::SetWalletDir(const fs::path& walletdir_path)
+{
+    gArgs.ForceSetArg("-walletdir", walletdir_path.string());
+}
